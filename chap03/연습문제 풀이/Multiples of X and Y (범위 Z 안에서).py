98306d62c015bec80707be9ca83f1ffb46972bd3
itminha123 <itminha123@naver.com>
2017-11-30 22:38:44 +0900

    어렵다 ㅠ

--- chap03/연습문제 풀이/Multiples of X and Y (범위 Z 안에서).py
+++ chap03/연습문제 풀이/Multiples of X and Y (범위 Z 안에서).py
@@ -1,14 +1,23 @@
 
 
 
-while True:
-    multiples=int(input("7과 9의 공배수"))
-    if multiples == 0:
-        break
-    else:
-        for i in range(10000):
-            if i % 7 == 0 and i % 9 == 0:
+while 1:
+    try:
+        start =int(input("공배수의 첫번째수를 입력해주세요:\n(0 <- 종료)"))
+        if start == 0:
+            print("쳇... bye")
+            break
+        end   =int(input("공배수의 두번째 수를 입력해주세요:"))
+        scope =int(input("구하고 싶은 공배수의 범위를 지정해주세요:"))
+
+        for i in range(scope):
+            if i % start == 0 and i % end == 0:
                 print(i)
+        print("구하고 싶은 공배수 입니다.")
+    except:
+        print("숫자만 입력하세요")
+
+
 
 
 

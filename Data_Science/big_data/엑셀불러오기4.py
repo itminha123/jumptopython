41556c7921ed36c17ffcbe17b9b032e463f5fc38
min <itminha123@naver.com>
2017-12-28 17:18:09 +0900

    어렵다

--- Data_Science/big_data/엑셀불러오기4.py
+++ Data_Science/big_data/엑셀불러오기4.py
@@ -1,3 +1,62 @@
+import math
+def my_sum(row_name):
+    result= 0
+    for i in row_name:
+        result += int(i)
+    print(result)
 
-def print_row(row_instance,type='int'):
+def my_average(row_name):
+    result =0
+    for i in row_name:
+        result += int(i)
+    print(result/len(row_name))
+
+def my_max(row_name):
+    return max(row_name)
+
+def my_min(row_name):
+    return min(row_name)
+
+def my_deviation(row_name):
+    result =0
+    for i in row_name:
+        result += int(i)
+    average=result/len(row_name)
+
+    for i in row_name:
+        deviation= int(i) - average
+    return  deviation
+
+def my_variance(row_name):
+    result =0
+    for i in row_name:
+        result += int(i)
+    average=result/len(row_name)
+    vari_sum = 0
+    for i in row_name:
+        vari_sum += ((int(i) - average)**2)
+    variance = vari_sum/len(row_name)
+    return row_name
+
+def my_standard_deviation(row_name):
+    result =0
+    for i in row_name:
+        result += int(i)
+    average=result/len(row_name)
+    vari_sum = 0
+    for i in row_name:
+        vari_sum += ((int(i) - average)**2)
+    variance = vari_sum/len(row_name)
+    standard_deviation = math.sqrt(variance)
+    return row_name
+
+def ascendeant_descendent(row_name):
+    for i in row_name:
+        print(sorted(i),end=' ')
+    for j in row_name:
+        print(j,end=' ')
     print()
+
+
+
+
